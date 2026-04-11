e6240b1b38f62b9cda2d836087d9201c0065d124
ZohhaKashif <bscs24126@itu.edu.pk>
2026-04-11 11:28:56 -0700

    Update CreateCriminal.js

--- frontend/src/components/CreateCriminal.js
+++ frontend/src/components/CreateCriminal.js
@@ -57,7 +57,7 @@ function CreateCriminal() {
     formData.append('relations', JSON.stringify(relationsArray));
 
     try {
-      const response = await fetch('https://fir-management-system.onrender.com/api/criminal', {
+      const response = await fetch(https://fir-management-system-0x79.onrender.com/api/criminal', {
         method: 'POST',
         headers: { 'Authorization': `Bearer ${token}` },
         body: formData,
